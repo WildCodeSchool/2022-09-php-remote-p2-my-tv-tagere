5b76645292ccfa018721b1f57cf40dbc5928b273
e-riff <emeric.riff@gmail.com>
2022-10-27 17:37:17 +0200

    fixed media queries, border-radius and height

--- public/assets/scripts/script.js
+++ public/assets/scripts/script.js
@@ -1,5 +1,11 @@
+//Ciblage des carousels
 const carousel = document.querySelectorAll(".carousel-items");
 
+//Ciblage des cards
+const lastCard = document.querySelectorAll("carousel-item");
+
+//Flag de l'affichage si besoin
+const isDisplayedLastCard = false;
 
 
 for (let i = 0; i < carousel.length; i++) {
@@ -9,3 +15,6 @@ for (let i = 0; i < carousel.length; i++) {
         carousel[i].scrollLeft += event.deltaY;
     })
 };
+
+
+
