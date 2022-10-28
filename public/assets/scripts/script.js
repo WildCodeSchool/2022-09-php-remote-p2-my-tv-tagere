bb4a6e63bb2653416d7b0a007fab01b6a8abc3d1
e-riff <emeric.riff@gmail.com>
2022-10-28 14:06:24 +0200

    Comportement du scroll de carroussel changé à la demande du Saint Client JF xD

--- public/assets/scripts/script.js
+++ public/assets/scripts/script.js
@@ -4,23 +4,12 @@ const carousels = document.querySelectorAll(".carousel-series");
 //Ciblage des cards
 const cards = document.querySelectorAll(".carousel-serie");
 
-/*
 for (let i = 0; i < carousels.length; i++) {
-    for (let j = 0; j < 4; j++) {
+    for (let j = (0 + (i * 4)); j < ((i * 4) + 4); j++) {
         cards[j].addEventListener("wheel", (event) => {
             event.preventDefault();
             console.log(event.deltaY)
             carousels[i].scrollLeft += event.deltaY;
         })
     }
-};
-*/
-
-
-for (let i = 0; i < carousels.length; i++) {
-    carousels.addEventListener("wheel", (event) => {
-        event.preventDefault();
-        console.log(event.deltaY)
-        carousels[i].scrollLeft += event.deltaY;
-    });
 };
