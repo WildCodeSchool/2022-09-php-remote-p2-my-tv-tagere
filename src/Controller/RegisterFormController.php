5dee1289ea04cbde04f20ddd2374213508bd82ec
FlorianBetin <florian.betin.pro@gmail.com>
2022-10-29 10:46:59 +0200

    Ajout d'un layout intermediaire pour la page landing dans le fichier Layouts. Supression MDP.

--- src/Controller/RegisterFormController.php
+++ src/Controller/RegisterFormController.php
@@ -3,19 +3,30 @@
 namespace App\Controller;
 
 use App\Model\RegisterManager;
-use App\Model\Connection;
 
 class RegisterFormController extends AbstractController
 {
     private RegisterManager $registerModel;
 
 
+
+    // Je n'arrive pas à intitialiser la paramètre du constructeur dans les méthodes ci-dessous.
+    //On me signale toujours une erreur. Pourrait on revoir çs ensemble ?
+    // Dans l'ItemController, la nouvelle lasse est initialisée dans chaque méthode, sans passer par le contruct.
+    // Je suis assez perplexe.
+
+    // public function __construct()
+    // {
+
+    //     $this->registerModel = new RegisterManager();
+
+    // }
+
+
     public function displayRegister(): string
     {
-        if ($_SERVER["REQUEST_METHOD"] === 'POST') {
-            //inscription utilisateur
-            $this->registerModel = new RegisterManager();
-        }
+
+        $this->registerModel = new RegisterManager();
         return $this->twig->render('FormConnect/register.html.twig');
     }
 
@@ -53,14 +64,8 @@ class RegisterFormController extends AbstractController
         if (empty($user['lastname']) || $user['firstname'] <= 2) {
             $errors[] = 'Votre nom de famille est obligatoire';
         }
-        if (empty($user['email'])) {
-            $errors[] = 'Votre email est obligatoire';
-        }
         if (empty($user['birthdate'])) {
             $errors[] = 'Votre date de naissance est obligatoire';
-        }
-        if (empty($user['password'])) {
-            $errors[] = 'Un mot de passe est obligatoire';
         }
         return $errors;
     }
