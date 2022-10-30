b042bc91365829cd9bb942530500325a7d0ff781
FlorianBetin <florian.betin.pro@gmail.com>
2022-10-30 16:21:02 +0100

    page seriedetail ajoutee et fonctionelle

---
+++ src/Controller/SeriePageController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\SeriePageManager;
+
+class SeriePageController extends AbstractController
+{
+
+private SeriePageManager $seriemodel;
+
+
+public function index(): string
+{
+    $this->seriemodel = new SeriePageManager();
+    $serie = $this->seriemodel->selectOneById($_GET["id"]);
+    return $this->twig->render('SeriePage/index.html.twig', [
+        'serie' => $serie,
+    ]);
+}
+}
