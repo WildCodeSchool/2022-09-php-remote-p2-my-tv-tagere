25b887e89956a4922a2de332b1b7d40e6a1a5b20
e-riff <emeric.riff@gmail.com>
2022-11-14 23:59:38 +0100

    adding show API for adding series

--- public/assets/scripts/addScript.js
+++ public/assets/scripts/addScript.js
@@ -8,3 +8,65 @@ function selectiveCheck(event) {
     if (checkedChecks.length >= maxCheckedBoxes + 1)
         return false;
 }
+
+
+
+function fetchSeries(serieSearch) {
+    axios.get(`https://api.tvmaze.com/search/shows?q=${serieSearch}`)
+        .then(function (response) {
+            return response.data;
+        })
+        .then(function (series) {
+            //console.log(JSON.stringify(series, null, 2));
+            let seriesHtml = `<section class="carouselSerie">
+				<ul class="carousel-series">`;
+            series.forEach((serie) => {
+                seriesHtml += `
+						<li class="carousel-serie">
+								<img src="${serie.show.image.original ? serie.show.image.original : " "}"/>
+                                <div class= "card-content">
+								<h3 class="card-title">${serie.show.name}</h3>
+                                <input type="button" id="completeForm" value="add" onclick="completeForm(${serie.show.id})">
+							</div>
+						</li >
+                `;
+            });
+
+            seriesHtml += `</ul >
+			</section > `;
+
+            document.querySelector('.fetchedSeries').innerHTML = seriesHtml;
+        });
+};
+
+function fetchOneSerie(id) {
+    axios.get(`https://api.tvmaze.com/shows/${id}`)
+        .then(function (response) {
+            return response.data;
+        })
+        .then(function (completeForm) {
+            //console.log(JSON.stringify(completeForm, null, 2));
+            let description
+            if (completeForm.summary != null) {
+                description = completeForm.summary.slice(3, -4);
+            } else {
+                description = "";
+            }
+            document.getElementById('serieName').setAttribute('value', completeForm.name);
+            document.getElementById('year').setAttribute('value', completeForm.premiered);
+            document.querySelector('#description').innerHTML = `${description}`;
+        });
+};
+
+
+
+
+
+function lokkingForSeries() {
+    let serieSearch = document.getElementById("serieSearch").value;
+    fetchSeries(serieSearch);
+}
+
+function completeForm(id) {
+    fetchOneSerie(id);
+}
