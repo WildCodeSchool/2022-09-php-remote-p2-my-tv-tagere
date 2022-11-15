8fc4d0a3369c2077f9c0bea18db4746e46542472
FlorianBetin <florian.betin.pro@gmail.com>
2022-11-15 17:48:55 +0100

    update series

--- src/Controller/SeriePageController.php
+++ src/Controller/SeriePageController.php
@@ -7,11 +7,40 @@ use App\Model\SeriePageManager;
 class SeriePageController extends AbstractController
 {
     private SeriePageManager $seriemodel;
+    public const TABLE = "serie";
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->seriemodel = new SeriePageManager();
+    }
 
     public function index(): string
     {
-        $this->seriemodel = new SeriePageManager();
+
         $serie = $this->seriemodel->selectOneById($_GET["id"]);
         return $this->twig->render('SeriePage/index.html.twig', ['serie' => $serie,]);
+    }
+
+    public function edit(int $id): ?string
+    {
+
+        $serie = 'serie';
+        $serieUpdate = $this->seriemodel->selectOneById($id);
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $_POST['id'] = $_GET['id'];
+            $serieUpdate = array_map('trim', $_POST);
+
+
+            $this->seriemodel->update($serieUpdate);
+
+            header('Location: /seriepage?id=' . $id);
+            return null;
+        } else {
+            return $this->twig->render('SeriePage/update.html.twig', [
+                'serie' => $serie,
+            ]);
+        }
     }
 }
--- src/Model/SeriePageManager.php
+++ src/Model/SeriePageManager.php
@@ -2,7 +2,29 @@
 
 namespace App\Model;
 
+use PDO;
+
 class SeriePageManager extends AbstractManager
 {
     public const TABLE = 'serie';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+
+    public function update(array $serieUpdate): bool
+    {
+        $statement = $this->pdo->prepare("UPDATE " . self::TABLE . " SET `name` = :serieName, `year` = :year,
+    `nb_of_seasons` = :nbOfSeasons, `description` = :description WHERE id=:id");
+
+        $statement->bindValue(':id', $serieUpdate['id'], PDO::PARAM_STR);
+        $statement->bindValue(':serieName', $serieUpdate['serieName'], PDO::PARAM_STR);
+        $statement->bindValue(':year', $serieUpdate['year'], PDO::PARAM_STR);
+        $statement->bindValue(':nbOfSeasons', $serieUpdate['nbOfSeasons'], PDO::PARAM_INT);
+        $statement->bindValue(':description', $serieUpdate['description'], PDO::PARAM_STR);
+
+        return $statement->execute();
+    }
 }
