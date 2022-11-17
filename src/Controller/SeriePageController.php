91241d51b60937a57ecf13715af50d774cd118cb
e-riff <emeric.riff@gmail.com>
2022-11-17 16:54:04 +0100

    correcting update +css

--- src/Controller/SeriePageController.php
+++ src/Controller/SeriePageController.php
@@ -28,17 +28,17 @@ class SeriePageController extends AbstractController
 
     public function edit(int $id): ?string
     {
-
-        $serie = 'serie';
-        $serieUpdate = $this->seriemodel->selectOneById($id);
+        $serie = $this->seriemodel->selectOneById($id);
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $_POST['id'] = $_GET['id'];
             $serieUpdate = array_map('trim', $_POST);
-
-
+            $serieUpdate['year'] = (is_numeric($serieUpdate['year']) && intval($serieUpdate['year']) > 1) ?
+                intval($serieUpdate['year']) : $serie["year"];
+            $serieUpdate['nbOfSeasons'] = (is_numeric($serieUpdate['nbOfSeasons']) &&
+                intval($serieUpdate['nbOfSeasons']) > 1) ?
+                intval($serieUpdate['nbOfSeasons']) : $serie["nb_of_seasons"];
+            $serieUpdate['id'] = $id;
             $this->seriemodel->update($serieUpdate);
-
             header('Location: /seriepage?id=' . $id);
             return null;
         } else {
